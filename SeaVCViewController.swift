5c40276668991c40a9387263e7770ec44be67cb4
Student <s525817@ch140007m.nwmissouri.edu>
2016-11-14 13:39:19 -0600

    Added EhVC,BeeVC and SeaVC

---
+++ SeaVCViewController.swift
@@ -0,0 +1,35 @@
+//
+//  SeaVCViewController.swift
+//  Git-ICE-VA
+//
+//  Created by Veerapaneni,Anurag on 11/14/16.
+//  Copyright © 2016 Veerapaneni,Anurag. All rights reserved.
+//
+
+import UIKit
+
+class SeaVCViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
